49a1457b8a1f6583f2ab4360b1e4271aa1499c8f
arataohtsuka-tech <arata_ohtsuka@iCloud.com>
2025-12-26 19:17:47 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Firebaseコンソールからコピーした設定をここに貼り付けます
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCqQxEwxZ6SAIF76SUbFxXG_LX7S8HT3WQ",
+  authDomain: "demon-duel-5639f.firebaseapp.com",
+  projectId: "demon-duel-5639f",
+  storageBucket: "demon-duel-5639f.firebasestorage.app",
+  messagingSenderId: "436932281654",
+  appId: "1:436932281654:web:03c9f402df2b66d32a9f97"
+};
